0b860fb3f1840dd1d4018a52cee695f35add9dbd
lhy <etwge@qq.com>
2018-05-02 11:36:32 +0800

    修复设置maxLines不起作用的问题

--- app/src/main/java/etwge/yu/linespaceextracompat/LineSpaceExtraCompatTextView.java
+++ app/src/main/java/etwge/yu/linespaceextracompat/LineSpaceExtraCompatTextView.java
@@ -8,10 +8,11 @@ import android.text.Layout;
 import android.util.AttributeSet;
 import android.util.Log;
 
-public class LineSpaceExtraCompatTextView extends AppCompatTextView implements IGetLineSpaceExtra{
+public class LineSpaceExtraCompatTextView extends AppCompatTextView implements IGetLineSpaceExtra {
 
 	private static final String TAG = LineSpaceExtraCompatTextView.class.getSimpleName();
-	private Rect mRect;
+	private Rect mLastLineShowRect;
+	private Rect mLastLineActualIndexRect;
 
 	public LineSpaceExtraCompatTextView(Context context) {
 		this(context, null);
@@ -23,7 +24,8 @@ public class LineSpaceExtraCompatTextView extends AppCompatTextView implements I
 
 	public LineSpaceExtraCompatTextView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-		mRect = new Rect();
+		mLastLineShowRect = new Rect();
+		mLastLineActualIndexRect = new Rect();
 	}
 
 	@Override
@@ -37,13 +39,20 @@ public class LineSpaceExtraCompatTextView extends AppCompatTextView implements I
 	 */
 	public int calculateExtraSpace() {
 		int result = 0;
-		int lastLineIndex = getLineCount() - 1;
+		//界面显示的最后一行的index
+		int lastLineShowIndex = Math.min(getMaxLines(), getLineCount()) - 1;
+		//实际上的最后一行的index，当没设置maxLines时，跟lastLineShowIndex的值相等
+		int lastLineActualIndex = getLineCount() - 1;
 
-		if (lastLineIndex >= 0) {
+		if (lastLineShowIndex >= 0) {
 			Layout layout = getLayout();
-			int baseline = getLineBounds(lastLineIndex, mRect);
-			if (getMeasuredHeight() == getLayout().getHeight()) {
-				result = mRect.bottom - (baseline + layout.getPaint().getFontMetricsInt().descent);
+			int baseline = getLineBounds(lastLineShowIndex, mLastLineShowRect);
+			getLineBounds(lastLineActualIndex, mLastLineActualIndexRect);
+			//只有测量的高度跟，getLayout的高度相等时这种情况时最后一行才多出的行间距
+			//因为有当设置maxLines时，通过实际最后一行的底部坐标减去显示最后一样的底部坐标得出看不见那部分的高度
+			//然后判断测量的高度，跟文字的总高度减去看不见的那部分高度，相等才去算最后一行多出的行间距的高，不相等说明TextView没有底部空白间隙
+			if (getMeasuredHeight() == getLayout().getHeight() - (mLastLineActualIndexRect.bottom - mLastLineShowRect.bottom)) {
+				result = mLastLineShowRect.bottom - (baseline + layout.getPaint().getFontMetricsInt().descent);
 			}
 
 		}
